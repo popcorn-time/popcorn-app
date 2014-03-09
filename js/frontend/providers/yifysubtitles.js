e554dc2ddc5d061fc2af59ba880c77df07cfa3eb
HeartRegaLz <micimaos@gmail.com>
2014-03-10 00:06:29 +0100

    arabic subtitle

--- js/frontend/providers/yifysubtitles.js
+++ js/frontend/providers/yifysubtitles.js
@@ -15,7 +15,8 @@ var request = require('request'),
         'romanian'  : 'Română',
         'portuguese': 'Português',
         'brazilian' : 'Português-Br',
-        'dutch'     : 'Nederlands'
+        'dutch'     : 'Nederlands',
+        'arabic'    : 'العربيه',
     };
 
 App.findSubtitle = function (model, cb, isFallback) {
