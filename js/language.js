19f70febfed9e4ceb12c188d77f74588eb0de339
Zheng Shao <axot@msn.com>
2014-03-13 02:56:50 +0900

    fixed a prefix bug

--- js/language.js
+++ js/language.js
@@ -2,7 +2,7 @@
 i18n.configure({
     defaultLocale: 'en',
     locales: ['ar', 'da', 'de', 'en', 'es', 'eu', 'fr', 'gr', 'he', 'it', 'ja', 'kr',
-              'lt', 'nl', 'no', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'sv', 'tr', 'cn'],
+              'lt', 'nl', 'no', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'sv', 'tr', 'zh'],
     directory: './language'
 });
 
